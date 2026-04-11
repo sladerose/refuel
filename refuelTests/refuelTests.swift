4b0d74371d50d3b188c7ee0369267048d4cd1d62
Slade Rose <sladerose1@gmail.com>
2026-04-11 07:04:39 +0200

    Initial Commit

---
+++ refuelTests/refuelTests.swift
@@ -0,0 +1,19 @@
+//
+//  refuelTests.swift
+//  refuelTests
+//
+//  Created by slade on 2026/04/11.
+//
+
+import Testing
+@testable import refuel
+
+struct refuelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
